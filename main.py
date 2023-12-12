b8ad58e349a57a0d52ccd4701499a14de6ae4bdc
nischayhegde <74798303+nischayhegde@users.noreply.github.com>
2023-12-12 00:15:06 -0600

    .

--- main.py
+++ main.py
@@ -28,12 +28,17 @@ run = client.beta.threads.runs.create(
 )
 
 
-"""
-run = client.beta.threads.runs.retrieve(
-  thread_id=thread.id,
-  run_id=run.id
-)
-"""
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+
+wait_on_run(run, thread)
 
 messages = client.beta.threads.messages.list(
   thread_id=thread.id
